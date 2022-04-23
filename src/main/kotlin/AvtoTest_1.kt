d2cfeb0f9a1c11ebf533a24c44fb1bf63bb7ff28
Vladimir Chistiakov <stomalux@yandex.ru>
2022-04-23 17:54:31 +0300

    Initial commit первый сомит. все работает

---
+++ src/main/kotlin/AvtoTest_1.kt
@@ -0,0 +1,65 @@
+// Определение комиссии перевода и его возможности
+const val commisConst = 0.0075 // Комиссия - 0.75%
+const val commisConstM = 0.006 // Комиссия - 0.6%
+const val commisMin: Int = 35_00   // Минимальная комиссия
+const val commisD: Int = 20_00   //  комиссия +20 руб
+var tipKard: String = "1"
+var ok: Boolean = true
+var comisKop = 0
+
+////////////////////////////////////////////////////////////////////////
+fun main() {
+//////////////////////////////
+    do {
+        println(
+            "Введите тип карты:\n  Mastercard,\n" + "  Maestro,\n" + "  Visa,\n" + "  Mir,\n" + "  VK_Pay:"
+        )
+        tipKard = readln()
+    } while (tipKard != "Mastercard" && tipKard != "Maestro" && tipKard != "Visa" && tipKard != "Mir" && tipKard != "VK_Pay")
+    print("Введите сумму предыдущего перевода в копейках: ")
+    val summOld = readln().toInt()
+    print("Введите сумму перевода в копейках: ")
+    val summNew = readln().toInt()
+///////////////////////////////
+    if (summNew <= 150_000_00 && (summNew + summOld) <= 600_000_00) {
+        comisKop = commission(tipKard, summOld, summNew)
+    } else {
+        ok = false
+        comisKop = 0
+        println("Превышен суточный/месячный лимит по карте!")
+    }
+///////////////////////////////
+    if (ok) println("Итоговая сумма комиссиии в копейках: $comisKop")
+    else println("Перевод отклонен!")
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+fun commission(tipKard: String, summOld: Int, summNew: Int): Int {
+    var comissionKop = 0
+    when (tipKard) {
+        "VK_Pay" -> {
+            println(tipKard)
+            if (summNew > 15_000_00 || (summNew + summOld) > 40_000_00) {
+                println("Превышен суточный/месячный лимит по аккаунту!")
+                ok = false
+                comissionKop = -1
+            } else comissionKop = 0
+        }
+
+        "Maestro", "Mastercard" -> {
+            println(tipKard)
+            if ((summNew + summOld) < 75_000_00) {
+                println("комиссии нет!")
+                comissionKop = 0
+                ok = true
+            } else comissionKop = (summNew * commisConstM + commisD).toInt()
+        }
+
+        "Visa", "Mir" -> {
+            println(tipKard)
+            val commissionOtSum = (summNew * commisConst).toInt()
+            comissionKop = if (commissionOtSum <= commisMin) commisMin else commissionOtSum
+        }
+    }
+    return comissionKop
+}
---
+++ src/test/kotlin/ru/netology/AvtoTest_1KtTest.kt
@@ -0,0 +1,228 @@
+package ru.netology
+
+import commission
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AvtoTest_1KtTest {
+
+    @Test
+    fun commission_VK_Pay_Norm() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "VK_Pay"
+        val summOldTest: Int = 10_000_00
+        val summNewTest: Int = 15_000_00
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun commission_VK_Pay_Nev() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "VK_Pay"
+        val summOldTest: Int = 10
+        val summNewTest: Int = 16_000_00
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(-1, result)
+    }
+
+    @Test
+    fun commission_VK_Pay_Old() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "VK_Pay"
+        val summOldTest: Int = 35_000_00
+        val summNewTest: Int = 10_000_00
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(-1, result)
+    }
+
+    @Test
+    fun commission_Maestro_Min() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "Maestro"
+        val summOldTest: Int = 10_00
+        val summNewTest: Int = 74_000_00
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun commission_Maestro_Normal() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "Maestro"
+        val summOldTest: Int = 100_000_00
+        val summNewTest: Int = 74_000_00
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(46400, result)
+    }
+    @Test
+    fun commission_Mastercard_Min() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "Mastercard"
+        val summOldTest: Int = 10_00
+        val summNewTest: Int = 74_000_00
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun commission_Mastercard_Normal() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "Mastercard"
+        val summOldTest: Int = 100_000_00
+        val summNewTest: Int = 74_000_00
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(46400, result)
+    }
+
+    @Test
+    fun commission_Visa_Ok() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "Visa"
+        val summOldTest: Int = 100_000_00
+        val summNewTest: Int = 100_000_00
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(75000, result)
+    }
+
+    @Test
+    fun commission_Visa_Min() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "Visa"
+        val summOldTest: Int = 100_000_00
+        val summNewTest: Int = 100
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(3500, result)
+    }
+    @Test
+    fun commission_Mir_Ok() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "Mir"
+        val summOldTest: Int = 100_000_00
+        val summNewTest: Int = 100_000_00
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(75000, result)
+    }
+
+    @Test
+    fun commission_Mir_Min() {
+
+        // Arrange — подготовка данных (задание переменных).
+        val tipKardTest: String = "Mir"
+        val summOldTest: Int = 100_000_00
+        val summNewTest: Int = 100
+
+        //  Act — выполнение целевого действия (вызов функции).
+        val result = commission(
+
+            tipKard = tipKardTest,
+            summOld = summOldTest,
+            summNew = summNewTest
+        )
+
+        //  Assert — сравнение ожидаемого результата с фактическим.
+        assertEquals(3500, result)
+    }
+
+}
